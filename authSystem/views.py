3508c12f1c1c31569856f2ca9b63bc3b04f881d2
Marcin <badenski.marcin@gmail.com>
2023-10-07 14:56:54 +0200

    weryfikacja przy rejestracji

--- authSystem/views.py
+++ authSystem/views.py
@@ -1,4 +1,33 @@
 from django.shortcuts import render
+from .forms import RegisterForm
+from django.contrib.auth.models import User
+from .utils import check_email
 
 def register(request):
-    return render(request, "register.html")
+    if request.method == 'GET':
+        return render(request, 'register.html', {'form': RegisterForm()})
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+
+        if password1 == password2:
+            username_taken = User.objects.filter(username=username).exists()
+            email_taken = User.objects.filter(email=email).exists()
+
+            if username_taken:
+                error = 'This username is taken. Try again!'
+            elif email_taken:
+                error = 'This e-mail is taken. Try again!' 
+            else:
+                email_valid = check_email(email)  
+                if email_valid:
+                    pass
+                else:
+                    error = 'Invalid e-mail. try again!'  
+        else:
+            error = 'Passwors must match. Try again!'
+            
+    return render(request, 'register.html',{'form':RegisterForm(), 'error': error})
+        
